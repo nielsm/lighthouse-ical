a2eea0022ea92295ed900ffe35468022629c91d9
Niels Meersschaert <nmeersschaert@mac.com>
2010-07-10 15:54:44 -0400

    Basic implementation of retrieving milestones.  No error handling present

---
+++ lib/lighthouse-ical.rb
@@ -0,0 +1,40 @@
+require 'lighthouse-api'
+require 'icalendar'
+
+class LighthouseIcal
+
+  #Returns an ics formatted string of all the milestones in the project
+  def self.create_calendar_for_project_id(project_id)
+    project = Lighthouse::Project.find(project_id)
+    calendar = Icalendar::Calendar.new
+    milestones = scheduled_milestones_in_project(project_id)
+    milestones.each do |ms|
+      # Retrieve the due_on value for each milestone & stip the time component
+      #Retrieve the title of the milestone & set it to the summary
+      #Retrieve the goals of the milestone & set it to the description
+      calendar.event do
+        dtstart       ms.due_on.to_date
+        dtend         ms.due_on.to_date
+        summary     ms.title
+        description ms.goals
+      end
+    end
+    calendar.publish
+    return calendar.to_ical
+  end
+  
+  private 
+  
+  
+  def self.milestones_in_project(project_id)
+    project = Lighthouse::Project.find(project_id)
+    return project.milestones
+  end
+
+  def self.scheduled_milestones_in_project(project_id)
+    raw_milestones = self.milestones_in_project(project_id)
+    scheduled = raw_milestones.select{|milestone|!milestone.due_on.blank?}.sort!{|a,b|a.due_on <=> b.due_on}
+  end
+
+
+end
--- lighthouse-ical.gemspec
+++ lighthouse-ical.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{lighthouse-ical}
-  s.version = "0.0.0"
+  s.version = "0.0.1"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Niels Meersschaert"]
-  s.date = %q{2010-07-09}
+  s.date = %q{2010-07-10}
   s.description = %q{Generate iCal feeds from your Lighthouse Scheduled Milestones}
   s.email = %q{niels@meerwerks.com}
   s.extra_rdoc_files = [
